e750791132dda3ffad26d0e5cede3a3132342bdb
James <zuma@zuma.eu.org>
2025-04-23 17:26:28 +0100

    Added socket initialization

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+
+// The default protocol for the other socket parameters
+static const int DEFAULT_PROTOCOL = 0;
+
+// Print the error message to stderr and exit if the assertion is false
+void assert(bool assertion, char *error) {
+  if (assertion)
+    return;
+  fprintf(stderr, "%s.\n", error);
+  exit(EXIT_FAILURE);
+}
 
 int main() {
-  printf("Hello, World!\n");
+  int sockfd = socket(AF_INET, SOCK_STREAM, DEFAULT_PROTOCOL);
+  assert(sockfd != -1, "Failed to create a socket");
+
   return EXIT_SUCCESS;
 }
